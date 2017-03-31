ec2ddbfd7068d4e478514e9f2c453c874aa66a64
Jeroen Hooyberghs <jeroen@hooyberghs.org>
2017-03-31 15:27:46 +0200

    rewrite tests according to ruby style guide

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,23 +7,23 @@ describe 'prometheus' do
         facts
       end
 
-      describe "Create the config file" do
-        context "with the defaults, using the tempate" do
+      describe 'Create the config file' do
+        context 'with the defaults, using the tempate' do
           it do
-            should contain_file("prometheus.yaml")
+            should contain_file('prometheus.yaml')
               .with(content: /---/)
           end
         end
-        context "when parameter config_type is source and and config_source is supplied" do
+        context 'when parameter config_type is source and and config_source is supplied' do
           let(:thesource) { File.expand_path(File.dirname(__FILE__) + '/../fixtures/files/prometheus.yaml') }
-          let(:params) {{ :config_type   => 'source', :config_source => thesource }}
+          let(:params) { { :config_type => 'source', :config_source => thesource } }
           it do
-            should contain_file("prometheus.yaml")
+            should contain_file('prometheus.yaml')
               .with(source: thesource)
           end
         end
-        context "with an invalid config_type" do
-          let(:params) {{ :config_type   => 'invalid' }}
+        context 'with an invalid config_type' do
+          let(:params) { { :config_type => 'invalid' } }
           it do
             expect {
               catalogue
